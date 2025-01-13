716ad6ae0cc29a4a82db1f6053c1191a3b46f385
nasekajlo <01184761@pw.edu.pl>
2025-01-13 23:28:22 +0100

    [CHANGED] Footer style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,19 +9,19 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <div className="fixed left-1/2 bottom-0 w-full transform -translate-x-1/2 max-w-4xl mx-auto py-4 flex justify-around items-center border-t bg-[rgb(20,20,20)]">
-            <button className="text-white text-lg flex flex-col items-center">
+            <button className="text-white text-3xl flex flex-col items-center">
                 🏠
             </button>
-            <button className="bg-gray-500 text-white text-lg rounded-full w-12 h-12 flex items-center justify-center">
+            <button className="text-white text-3xl rounded-full w-16 h-16 flex items-center justify-center">
                 ➕
             </button>
             <Link href="/chats">
-                <button className="text-white text-lg flex rounded-full items-center">
+                <button className="text-white text-3xl flex rounded-full items-center">
                     💬
                 </button>
             </Link>
             <Link href="/profile">
-                <button className="text-white text-lg flex rounded-full items-center">
+                <button className="text-white text-3xl flex rounded-full items-center">
                     👤
                 </button>
             </Link>
